8c4ee71f3d7f488713935c88c4a705bd84190b36
Thiago Bruno <tbgt35@hotmail.com>
2024-01-22 22:59:10 -0300

    Primeira versao terminada

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -5,20 +5,41 @@ import {
   TouchableOpacity,
   Pressable,
 } from "react-native";
+import * as Clipboard from "expo-clipboard";
+import useStorage from "../../hooks/useStorage";
 
 export function ModalPasword({ password, handleClose }) {
+  const { saveItem } = useStorage();
+
+  async function handleCopyPassword() {
+    await Clipboard.setStringAsync(password);
+    await saveItem("@pass", password);
+    alert("Senha salva com sucesso");
+
+    handleClose();
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.content}>
         <Text style={styles.title}>Senha Gerada</Text>
-        <Pressable style={styles.innerPassword}>
+
+        <Pressable
+          style={styles.innerPassword}
+          onLongPress={handleCopyPassword}
+        >
           <Text style={styles.innerText}>{password}</Text>
         </Pressable>
+
         <View style={styles.buttonArea}>
           <TouchableOpacity style={styles.button} onPress={handleClose}>
             <Text style={styles.buttonText}>Voltar</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={{ ...styles.button, ...styles.buttonSave }}>
+
+          <TouchableOpacity
+            style={{ ...styles.button, ...styles.buttonSave }}
+            onPress={handleCopyPassword}
+          >
             <Text style={styles.buttonSaveText}>Salvar senha</Text>
           </TouchableOpacity>
         </View>
